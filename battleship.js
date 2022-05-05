e1f5e6347c1c02471594bc378b0304f135819443
Maxim <maksim_mikhaylov1810@mail.ru>
2022-05-05 11:55:52 +0300

    add

--- battleship.js
+++ battleship.js
@@ -1,6 +1,6 @@
 var view = {
-  displayMessag: function(msg) {
-    var messageArea = document.getElementByld("messageArea");
+  displayMessage: function(msg) {
+    var messageArea = document.getElementById("messageArea");
     messageArea.innerHTML = msg;
   },
   displayHit: function(location) {
@@ -12,3 +12,11 @@ var view = {
     cell.setAttribute("class", "miss");
   }
 };
+
+view.displayMiss("00");
+view.displayHit("34");
+view.displayMiss("55");
+view.displayHit("12");
+view.displayMiss("25");
+view.displayHit("26");
+view.displayMessage("Tap tap, is this thing on?");
