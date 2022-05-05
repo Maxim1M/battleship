0c80c798df70368b86e36633ae365443154d2418
Maxim <maksim_mikhaylov1810@mail.ru>
2022-05-05 11:41:04 +0300

    add hit, miss

--- battleship.js
+++ battleship.js
@@ -5,8 +5,10 @@ var view = {
   },
   displayHit: function(location) {
     var cell = document.getElementById(location);
+    cell.setAttribute("class", "hit");
   },
   displayMiss: function(location) {
-
-  },
+    var cell = document.getElementById(location);
+    cell.setAttribute("class", "miss");
+  }
 };
