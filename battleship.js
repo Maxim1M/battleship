cdc2a83054628a5e16702a9cc50eb3d6b3ba3647
Maxim <maksim_mikhaylov1810@mail.ru>
2022-05-06 13:13:56 +0300

    add

--- battleship.js
+++ battleship.js
@@ -55,26 +55,29 @@ var model = {
 
 var controller = {
   guess: 0,
-  processGuess: function(guess) {
-    var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+  procesGuess: function (guess) {
+    function parseGuess (guess) {
+      var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
 
-    if (guess === null || guess.lenght !== 2) {
-      alert("Oops, please enter a letter and a number on the board.");
-    } else {
-      firstChar = guess.charAt(0);
-      var row = alphabet.indexOf(firstChar);
-      var column = guess.charAt(1);
-
-      if (isNaN(row) || isNaN(column)) {
-        alert("Oops, that isn't on the board.");
-      } else if (row < 0 || row >= model.boardSize ||column < 0 || column >= model.boardSize) {
-        alert("Oops, that's off the board!");
+      if (guess === null || guess.length !== 2) {
+        alert("Oops, please enter a letter and a number on the board.");
       } else {
-        return row + column;
+        firstChar = guess.charAt(0);
+        var row = alphabet.indexOf(firstChar);
+        var column = guess.charAt(1);
+
+        if (isNaN(row) || isNaN(column)) {
+          alert("Oops, that isn't on the board.");
+        } else if (row < 0 || row >= model.boardSize ||column < 0 || column >= model.boardSize) {
+          alert("Oops, that's off the board!");
+        } else {
+          return row + column;
+        }
       }
+      return null;
     }
-    return null;
   }
 };
+
 
 
