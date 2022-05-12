33a637848fd3a9134aef184667e4e79d6531b524
Maxim <maksim_mikhaylov1810@mail.ru>
2022-05-12 17:41:22 +0300

    add

--- battleship.js
+++ battleship.js
@@ -90,6 +90,30 @@ var controller = {
 	}
 }
 
+function init() {
+  var fireButton = document.getElementById("fireButton");
+  fireButton.onclick = handleFireButton;
+  var guessInput = document.getElementById("guessInput");
+  guessInput.onkeypress = handleKeyPress;
+}
+
+function handleKeyPress(e) {
+  var fireButton = document.getElementById("fireButton");
+  if (e.keyCode === 13) {
+    fireButton.click();
+    return false;
+  }
+}
+
+function handleFireButton() {
+  var guessInput = document.getElementById("guessInput");
+  var guess = guessInput.value;
+  controller.processGuess(guess);
+
+  guessInput.value = "";
+}
+
+window.onload = init;
 
 
 
